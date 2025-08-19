ac53125c4381d48a34d03ff38cabd566daea6736
mariane <sa.mariane08@aluno.ifce.edu.br>
2025-08-19 09:10:44 -0300

    Add files via upload

---
+++ Operadores-Aritmeticos/ArithmeticDemo.java
@@ -0,0 +1,52 @@
+package edu.br.principal;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+		// alguns números
+		int i = 37;
+		int j = 42;
+		double x = 27.475;
+		double y = 7.22;
+		
+		System.out.println("Valor das variaveis...");
+		System.out.println(" i = " +i);
+		System.out.println(" j = " +j);
+		System.out.println(" x = " +x);
+		System.out.println(" y = " +y);
+		
+		// adição dos números
+		System.out.println("Adição...");
+		System.out.println(" i + j = " + (i + j));
+		System.out.println(" x + y = " + (x + y));
+		
+		// Subtração
+		System.out.println("Subtração...");
+		System.out.println(" i - j = " + (i - j));
+		System.out.println(" x - y = " + (x - y));
+		
+		// multiplicação dos números
+		System.out.println("Multiplicação...");
+		System.out.println(" i * j = " + (i * j));
+		System.out.println(" x * y = " + (x * y));
+		
+		// Divisão dos números
+		System.out.println("divisao...");
+		System.out.println(" i * j = " + (i * j));
+		System.out.println(" x * y = " + (x * y));
+		
+		// resto da divisão
+		System.out.println("RestoDiv...");
+		System.out.println(" i % j = " + (i % j));
+		System.out.println(" x % y = " + (x % y));
+		
+		// misturando operações
+		System.out.println("Valor maximo dos tipos...");
+		System.out.println(" j + y = " + (j + y));
+		System.out.println(" i * x = " + (i * x));
+		
+		
+
+	}
+
+}
